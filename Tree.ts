e4631e98bcb06a957fb4d937c5baa8175936e2c7
Ahmed Ibrahim <ib153507@gmail.com>
2021-04-01 18:22:12 +0200

    Create The insert method

--- Tree.ts
+++ Tree.ts
@@ -10,6 +10,41 @@ class NodeClass {
 
 class Tree {
   root = null;
+
+  insert(value: number | string) {
+    const node = new NodeClass(value);
+    if (this.root === null) {
+      this.root = node;
+    } else {
+      let current = this.root;
+      while (current) {
+        if (value < current.value) {
+          if (current.left) {
+            current = current.left;
+          } else {
+            current.left = node;
+            return;
+          }
+        } else {
+          if (current.right) {
+            current = current.right;
+          } else {
+            current.right = node;
+            return;
+          }
+        }
+      }
+    }
+    return node;
+  }
 }
 
-
+const tree = new Tree();
+tree.insert(7);
+tree.insert(4);
+tree.insert(9);
+tree.insert(1)
+tree.insert(6)
+tree.insert(8)
+tree.insert(10)
+console.log(tree);
