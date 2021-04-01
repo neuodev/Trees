b623843cacbe0896fbf8c0af1ea4e033105241af
Ahmed Ibrahim <ib153507@gmail.com>
2021-04-01 18:06:49 +0200

    Create the Node calss and the Tree class

---
+++ Tree.ts
@@ -0,0 +1,15 @@
+class NodeClass {
+  left;
+  right;
+  constructor(public value) {
+    this.value = value;
+    this.right = null;
+    this.left = null;
+  }
+}
+
+class Tree {
+  root = null;
+}
+
+
